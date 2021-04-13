7798bba435cfd492afdc9f4a3f2158e007e28afb
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-04-13 23:42:12 +0200

    Add files via upload

---
+++ Data_Preprocessing_Tool.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset= pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values #features
+y = dataset.iloc[:,-1].values  #Dependent values
+
+print(x)
+print(y)
+
+
+
+
